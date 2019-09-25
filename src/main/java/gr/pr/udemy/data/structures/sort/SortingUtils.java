3a02322d43a979e35a18d8d6128aa513c7d461e0
Panagiotis Ressos <p.ressos@hotmail.com>
2019-09-25 14:34:09 +0300

    Minor refactor in SortingUtils.swapIntArray

--- src/main/java/gr/pr/udemy/data/structures/sort/SortingUtils.java
+++ src/main/java/gr/pr/udemy/data/structures/sort/SortingUtils.java
@@ -3,6 +3,10 @@ package gr.pr.udemy.data.structures.sort;
 public class SortingUtils {
 
 	public static void swapIntArray(int[] intArray, int i, int j) {
+		if (i == j) {
+			return;
+		}
+
 		int temp = intArray[j];
 		intArray[j] = intArray[i];
 		intArray[i] = temp;
